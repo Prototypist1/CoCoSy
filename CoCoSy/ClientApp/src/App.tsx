015ecd1781bdcd112d63c2bb999966ab51996393
Prototypist1 <ColinWielga@outlook.com>
2024-02-29 21:15:24 -0700

    whatever

--- CoCoSy/ClientApp/src/App.tsx
+++ CoCoSy/ClientApp/src/App.tsx
@@ -20,11 +20,85 @@ const voteLimit = 3;
 //const botGradient = "15, 118, 123";
 //const shadow = "1, 5, 6";
 
-const glow = "255, 255, 20";
-const topGradient = "247, 235, 50";
-const botGradient = "227, 133, 17";
-const shadow = "141,0,0";
+const glow = "242, 242, 242";
+const topGradient = "204, 204, 204";
+const botGradient = "41, 120, 160";
+const shadow = "36, 35, 49";
 
+// $white-smoke: rgba(242, 242, 242, 1);
+//$silver: rgba(204, 204, 204, 1);
+//$licorice: rgba(20, 8, 14, 1);
+//$raisin - black: rgba(36, 35, 49, 1);
+//$cerulean: rgba(41, 120, 160, 1);
+
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from https://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+function hslToRgb(h: number, s: number, l:number): [number, number, number]{
+    let r, g, b;
+
+    if (s === 0) {
+        r = g = b = l; // achromatic
+    } else {
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hueToRgb(p, q, h + 1 / 3);
+        g = hueToRgb(p, q, h);
+        b = hueToRgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function hueToRgb(p:number, q:number, t:number) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+}
+
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   {number}  r       The red color value
+ * @param   {number}  g       The green color value
+ * @param   {number}  b       The blue color value
+ * @return  {Array}           The HSL representation
+ */
+function rgbToHsl(r: number, g: number, b: number): [number, number, number] {
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const vmax = Math.max(r, g, b), vmin = Math.min(r, g, b);
+    let h, s, l = (vmax + vmin) / 2;
+
+    if (vmax === vmin) {
+        return [0, 0, l]; // achromatic
+    }
+
+    const d = vmax - vmin;
+    s = l > 0.5 ? d / (2 - vmax - vmin) : d / (vmax + vmin);
+    if (vmax === r) h = (g - b) / d + (g < b ? 6 : 0);
+    if (vmax === g) h = (b - r) / d + 2;
+    if (vmax === b) h = (r - g) / d + 4;
+    h /= 6;
+
+    return [h, s, l];
+}
 
 type Vote =
     {
@@ -478,7 +552,7 @@ function App() {
                                             })
                                         }
                                     }}>{"\uf137"}</Button>
-                                <Typography variant="h5" overflow="hidden" textOverflow="ellipsis" sx={{overflowWrap:"break-word"}} textAlign="center"> {option.name} </Typography> {/* <Typography variant="h6"> {(option.support / 1000).toFixed()}</Typography> */}
+                                <Typography variant="h5" overflow="hidden" textOverflow="ellipsis" sx={{ overflowWrap: "break-word" }} textAlign="center"> {option.name} </Typography> {/* <Typography variant="h6"> {(option.support / 1000).toFixed()}</Typography> */}
                                 <Button
                                     disabled={outOfVotes && !CanRetractVote(option.againsts)}
                                     onClick={() => {
@@ -511,7 +585,7 @@ function App() {
                             <Droppable droppableId={"+" + option.name} direction="horizontal">
                                 {(provided, snapshot) => (
                                     <Stack
-                                        sx={{ height: "100%", paddingBottom: 2}}
+                                        sx={{ height: "100%", paddingBottom: 2 }}
                                         padding={1}
                                         direction="row"
                                         justifyContent="flex-start"
@@ -547,11 +621,11 @@ function App() {
                     ]).flatMap(x => x)}
                 </Grid>
             </DragDropContext>
-            <input type="text" style={{ backgroundColor: `rgb(${shadow},0.1)`, border: 0, borderRadius: 5, boxShadow: `inset 0px 1px 3px rgb(${shadow},0.5)`, padding: 10 }}/>
+            <input type="text" style={{ backgroundColor: `rgb(${shadow},0.1)`, border: 0, borderRadius: 5, boxShadow: `inset 0px 1px 3px rgb(${shadow},0.5)`, padding: 10 }} />
             <TextField
                 value={state.toAdd}
                 onChange={(value) => actions.setToAdd(value.target.value)}
-                
+
             />
             <Button onClick={() => {
                 if (state.toAdd !== "") {
@@ -577,7 +651,7 @@ function App() {
             }}>Set Name</Button>
             <Button onClick={() => actions.clear()}>Clear</Button>
 
-            <div style={{padding: 10, borderRadius: 5, boxShadow: `inset 1px 1px 4px rgb(${glow},0.5), 0px 2px 7px rgb(${shadow},0.3), 0px 1px 2px rgb(${shadow},0.5)` }} > hello world
+            <div style={{ padding: 10, borderRadius: 5, boxShadow: `inset 1px 1px 4px rgb(${glow},0.5), 0px 2px 7px rgb(${shadow},0.3), 0px 1px 2px rgb(${shadow},0.5)` }} > hello world
             </div>
         </Stack>
     );
